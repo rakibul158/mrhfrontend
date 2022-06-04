595895493c0b62d18bf35c884e0fbae982a18b7c
rakibul158 <rakib3665@gmail.com>
2022-06-04 11:25:48 +0600

    setup responsive and create layout for nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,17 @@ class Header extends Component {
   render() {
     return (
       <div className='Header-Wapper'>
-        <h2>Header</h2>
+        <div className='Header-Container'>
+          <div className='Header-Start'>
+            <h3>mrhrakibul</h3>
+          </div>
+          <div className='Header-Center'>
+            <p>Navigation Here</p>
+          </div>
+          <div className='Header-End'>
+            <p>Contact Me</p>
+          </div>
+        </div>
       </div>
     )
   }

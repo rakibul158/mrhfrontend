4ad93ffb5e5e5199549bfe256b80d3e955f91a5f
rakibul158 <rakib3665@gmail.com>
2022-06-04 09:40:03 +0600

    Setup mrhrakiublHasan with resposive and install sass

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className='Header-Wapper'>
+        <h2>Header</h2>
+      </div>
+    )
+  }
+}
+
+export default Header;
---
+++ src/routes/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Header from '../../components/Header/Header';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+          <Header />
+      </div>
+    )
+  }
+}
+export default HomePage;
